91d9d0138b04cd335f59403eca2a863de5028a9f
3yassu <eyassuthegamemaster@gmail.com>
2025-01-31 12:41:00 -0600

    Replace the hello world program with the sample C program

--- hello.c
+++ hello.c
@@ -7,16 +7,16 @@ printf("Enter a string: ");
 gets(str);
 len = strlen(str);
 for (i = 0; i < len; i++) {
-if (str[i] >= 'a' && str[i] <= 'z') {
-str[i] = str[i] - 'a' + 'A';
+    if (str[i] >= 'a' && str[i] <= 'z') {
+    str[i] = str[i] - 'a' + 'A';
 }
 }
 printf("The capitalized string is: %s\n", str);
 // Reverse the string.
 for (i = 0, len = strlen(str); i < len / 2; i++) {
-char temp = str[i];
-str[i] = str[len - 1 - i];
-str[len - 1 - i] = temp;
+    char temp = str[i];
+    str[i] = str[len - 1 - i];
+    str[len - 1 - i] = temp;
 }
 printf("The reversed string is: %s\n", str);
 return 0;
